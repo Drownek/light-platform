299580d57899cd1b2a52e6b2b6d610a6d82df45e
Drownek <46686155+Drownek@users.noreply.github.com>
2024-11-22 20:32:28 +0100

    Add withoutCancel option to WaitingTask

--- bukkit-utils/src/main/java/pl/drownek/util/WaitingTask.java
+++ bukkit-utils/src/main/java/pl/drownek/util/WaitingTask.java
@@ -1,6 +1,5 @@
 package pl.drownek.util;
 
-import lombok.Builder;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.title.Title;
@@ -27,22 +26,27 @@ public class WaitingTask extends BukkitRunnable implements Listener {
     private final Runnable successAction;
     private final Runnable failAction;
     private final List<Listener> customListeners;
+    private final boolean withoutCancel;
 
     private Listener listener;
     private long finishState;
     private Player player;
 
-    @Builder
-    public WaitingTask(Plugin plugin, String actionName, Duration duration, Runnable successAction, Runnable failAction, List<Listener> customListeners) {
+    public WaitingTask(Plugin plugin, String actionName, Duration duration, Runnable successAction, Runnable failAction, List<Listener> customListeners, boolean withoutCancel) {
         this.actionName = actionName;
         this.duration = duration.toSeconds();
         this.successAction = successAction;
         this.failAction = failAction;
         this.plugin = plugin;
+        this.withoutCancel = withoutCancel;
         if (customListeners == null) {
             customListeners = new ArrayList<>();
         }
         this.customListeners = customListeners;
+    }
+
+    public static WaitingTaskBuilder builder() {
+        return new WaitingTaskBuilder();
     }
 
     public void start(Player player) {
@@ -52,7 +56,7 @@ public class WaitingTask extends BukkitRunnable implements Listener {
         this.listener = new Listener() {
             @EventHandler
             public void handle(PlayerToggleSneakEvent event) {
-                if (!event.getPlayer().equals(player)) {
+                if (!event.getPlayer().equals(player) || withoutCancel) {
                     return;
                 }
                 WaitingTask.this.cancel(false);
@@ -109,5 +113,61 @@ public class WaitingTask extends BukkitRunnable implements Listener {
         this.customListeners.forEach(it -> HandlerList.unregisterAll(it));
 
         this.cancel();
+    }
+
+    public static class WaitingTaskBuilder {
+        private Plugin plugin;
+        private String actionName;
+        private Duration duration;
+        private Runnable successAction;
+        private Runnable failAction;
+        private List<Listener> customListeners;
+        private boolean withoutCancel = false;
+
+        WaitingTaskBuilder() {
+        }
+
+        public WaitingTaskBuilder plugin(Plugin plugin) {
+            this.plugin = plugin;
+            return this;
+        }
+
+        public WaitingTaskBuilder actionName(String actionName) {
+            this.actionName = actionName;
+            return this;
+        }
+
+        public WaitingTaskBuilder duration(Duration duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public WaitingTaskBuilder successAction(Runnable successAction) {
+            this.successAction = successAction;
+            return this;
+        }
+
+        public WaitingTaskBuilder failAction(Runnable failAction) {
+            this.failAction = failAction;
+            return this;
+        }
+
+        public WaitingTaskBuilder customListeners(List<Listener> customListeners) {
+            this.customListeners = customListeners;
+            return this;
+        }
+
+        public WaitingTaskBuilder withoutCancel() {
+            this.withoutCancel = true;
+            return this;
+        }
+
+        public WaitingTask build() {
+            return new WaitingTask(this.plugin, this.actionName, this.duration, this.successAction, this.failAction, this.customListeners, this.withoutCancel);
+        }
+
+        public String toString() {
+            return "WaitingTask.WaitingTaskBuilder(plugin=" + this.plugin + ", actionName=" + this.actionName + ", duration=" + this.duration + ", successAction=" + this.successAction + ", failAction=" + this.failAction + ", customListeners=" + this.customListeners + ", withoutCancel=" + this.withoutCancel + ")";
+        }
     }
 }
